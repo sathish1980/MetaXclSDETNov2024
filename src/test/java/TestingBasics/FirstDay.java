48a21a1128a22e4da6d41373ebd10ff2b4790ca9
sathish1980 <kumar.sathish189@gmail.com>
2024-11-15 06:42:07 +0530

    secind class

--- src/test/java/TestingBasics/FirstDay.java
+++ src/test/java/TestingBasics/FirstDay.java
@@ -34,7 +34,7 @@ public class FirstDay {
 		 * id
 		 * name
 		 * classname
-		 * tagname
+		 * tagname --onHold
 		 * linktext
 		 * partiallinktext
 		 * cssselector
@@ -46,10 +46,57 @@ public class FirstDay {
 		
 		WebElement username1 = browser.findElement(By.name("email"));
 		username1.sendKeys("type");
+		username1.clear();
 		//username1.click();
 		
 		//WebElement username3 = browser.findElement(By.className("inputtext _55r1 _6luy"));
 		//username3.sendKeys("type");
+		
+		//browser.findElement(By.linkText("Forgotten password?")).click();
+		
+		//browser.findElement(By.partialLinkText("pass")).click();
+		
+		/*
+		 * Css Selector
+		 * Tag and  ID -- Tag#id
+		 * Tag and Class -- Tag.class
+		 * Tag and attribute -- Tag[attribute=value]
+		 * Tag,Class and Atrribute -- Tag.class[attribute=value]
+		 * Startswith -- [attribute^='staringvlaue of the attribute']
+		 * endswith -- [attribute$='end value of the attribute']
+		 * contains --[attribute*='any value of the attribute']
+		 */
+		
+		browser.findElement(By.cssSelector("input#email")).sendKeys("test");
+		
+		//browser.findElement(By.cssSelector("input.inputtext _55r1 _6luy")).clear();
+		
+		browser.findElement(By.cssSelector("input[data-testid='royal_email']")).clear();
+		
+		//browser.findElement(By.cssSelector("input.inputtext _55r1 _6luy[data-testid='royal_email']")).sendKeys("test");
+		
+		
+		/*
+		 * Xpath -XML Path
+		 * 2 types
+		 * absolute xpath -- extact path of the element
+		 * relative xpath -- some were in the DOM structure
+		 * 5 Types
+		 * Basic xpath --//tagname[@attribute='value']     or //*[@attribute='value']
+		 * Starts-with -- //tagname[starts-with(@attribute,'value')]     
+		 * contains -- //tagname[contains(@attribute,'value')]
+		 * and or
+		 * text
+		 */
+		
+		browser.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[2]/div/div/form/div/div/input")).sendKeys("Meta Xcel");
+		
+		browser.findElement(By.xpath("//input[@type='text']")).clear();
+	
+		browser.findElement(By.xpath("//input[starts-with(@placeholder,'Email')]")).sendKeys("yeah");
+	
+		browser.findElement(By.xpath("//*[starts-with(@placeholder,'Email')]")).sendKeys("yeah");
+
 	}
 
 }
